fd7a320150ec161500a3298aa6d4398658761fec
Vivek Yaligar <vkyaligar@gmail.com>
2024-06-22 07:51:28 +0530

    missing change

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   output: 'export',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/labfox' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/labfox' : '',
