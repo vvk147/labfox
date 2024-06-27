f399b743b9e5af72dc3fb934d54be019cbaf2ad5
Vivek Yaligar <30796905+vvk147@users.noreply.github.com>
2024-06-27 23:52:40 +0530

    :tada: Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
   output: 'export',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/labfox' : '',
   basePath: process.env.NODE_ENV === 'production' ? '/labfox' : '',
+  
 };
 
 module.exports = nextConfig;
